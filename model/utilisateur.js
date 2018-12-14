ea56abde4a8c4001b1b3e85de09d469a01ba6f64
Ghouibi <ghouibi.ghassen@gmail.com>
2018-12-14 01:16:01 +0100

    L'ajout de la fonction qui permet d'ajouter les post it dans la base de données

--- model/utilisateur.js
+++ model/utilisateur.js
@@ -17,12 +17,12 @@ class Database{
 
     constructor(){ 
     }
-    /*Fonction ajouterUnUtilisateurDansLaBaseDeDonnes(nom,prenom,email,motdepasse,done)
+    /** Fonction ajouterUnUtilisateurDansLaBaseDeDonnes(nom,prenom,email,motdepasse,done)
     *brief cette fonction permet l'ajout d'un nouveau utilisateur dans la base de données à condition qu'il a pas déjà un compte 
-    *param nom le nom renseigner de l'utilisateur
-    *param prenom le prenom renseigner par l'utilisateur
-    *param email l'email de l'utilisateur et ce dernier il peut pas exister 2 fois
-    *param done retourne 0 si le compte existe et 1 si le compte n'existe pas et il sera bien ajouté 
+    *@param nom le nom renseigner de l'utilisateur
+    *@param prenom le prenom renseigner par l'utilisateur
+    *@param email l'email de l'utilisateur et ce dernier il peut pas exister 2 fois
+    *@param done retourne 0 si le compte existe et 1 si le compte n'existe pas et il sera bien ajouté 
     */
     ajouterUnUtilisateurDansLaBaseDeDonnes(nom,prenom,email,motdepasse,done){
         var search="SELECT * FROM utilisateur WHERE email=?";
@@ -51,11 +51,11 @@ class Database{
             }
         });
     }
-    /* fonction chercherLutilisateurDansLabaseDeDonnees(email,motdepasse,done)
-    *brief cette fonction permet de rechercher si un utilisateur exsiste ou pas dans la basse de données
-    *param email l'email de l'utilisateur
-    *param motdepasse le mot de passe de l'utilisateur
-    *param done retourner la valeur dans la variable done
+    /**  fonction chercherLutilisateurDansLabaseDeDonnees(email,motdepasse,done)
+    * brief cette fonction permet de rechercher si un utilisateur exsiste ou pas dans la basse de données
+    * @param email l'email de l'utilisateur
+    * @param motdepasse le mot de passe de l'utilisateur
+    * @param done retourner la valeur dans la variable done
     */
    chercherLutilisateurDansLabaseDeDonnees(email,motdepasse,done){
        
@@ -79,7 +79,53 @@ class Database{
         
         });
     }
-    //TODO fonction ajouterLePostitAlaBaseDeDonnees
+    /** fonction chercherLidDeLutilisateur()
+     * brief cette fonction renvoie l'id de l'utilisateur
+     * @param email qui permet la requete a identifier l'utilisateur grace a son mail
+     */
+    chercherLidDeLutilisateur(email,done){
+        var search="SELECT id FROM utilisateur WHERE email=?";
+        connection.query(search,[email] ,function (err, rows) {   
+            if(rows[0]){
+                console.log('=>', rows[0].id);
+                done(rows[0].id);
+            }
+            
+            else{
+                done(0);
+            }
+        });
+
+
+    }
+    /** fonction ajouterLePostitAlaBaseDeDonnees()
+     *brief cette fonction permet d'ajouter les differents élément de post_it dans la base de données
+     * @param idUtilisateur l'id de l'utilisteur recupérable a partir du token
+     * @param coordonneesX le coordonées de post it par rapport à l'axe X
+     * @param coordonneesY le coordonées de post it par rapport à l'axe Y
+     * @param distance la distance de post it par rapport à l'origine
+     * @param angleX l'angle de post_it par rapport à l'ogirine
+     * @param tangente l'angle de post_it par rapport au radar (cercle du centre de l'origine)
+     * @param text le text de post_it
+     * @param couleur la couleur de post_it 
+     * @param done où on va reçevoir la valeur
+     */
+    ajouterLePostitAlaBaseDeDonnees(idUtilisateur,coordonneesX,coordonneesY,distance,angleX,tangente,text,couleur,done){
+
+        var insertQuery = "INSERT INTO post_it (id,coordonnees_x,coordonneees_y,distance,angleX,tangente,text,couleur) values (?,?,?,?,?,?,?,?)";
+
+        connection.query(insertQuery, [idUtilisateur,coordonneesX,coordonneesY,distance,angleX,tangente,text,couleur],function(err){
+            if (err) throw err;
+            else{
+                console.log("Post_it Ajouté");
+                done(1);
+            }
+        });
+    
+    }
+    /**fonction chargerLesPostitDeLaBaseDeDonnees
+     * 
+     */
 
 
 
