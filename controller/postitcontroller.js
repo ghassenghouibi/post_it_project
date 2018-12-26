46cf3b12b9a263de56dc4c37398003bf7bb5a02e
Ghouibi <ghouibi.ghassen@gmail.com>
2018-12-26 02:27:49 +0100

    Decomposition de l'ecran et calcul de l'angle du souris à l'instant t

--- controller/postitcontroller.js
+++ controller/postitcontroller.js
@@ -51,9 +51,11 @@ class Postit{
             couleur:randomColor()
         };
         do{
-            post_it.axeX=getRandomIntInclusive(0,1200);
-            post_it.axeY=getRandomIntInclusive(0,600);
-            console.log("Position ", post_it.axeX, post_it.axeY);
+            post_it.axeX=getRandomIntInclusive(-window.innerWidth,window.innerWidth*3/2);
+            post_it.axeY=getRandomIntInclusive(-window.innerHeight,window.innerHeight*3/2);
+            console.log("Position ->", post_it.axeX, post_it.axeY);
+            console.log("Centre de l'ecran ->",window.innerWidth/2,window.innerHeight/2);
+            console.log("distance ->",window.innerWidth/2-post_it.axeX,window.innerHeight/2-post_it.axeY);
 
         }while(!(verifierLacollision(post_it.axeX,post_it.axeY,tabx,taby)));
         tabx.push(post_it.axeX);
@@ -61,9 +63,26 @@ class Postit{
         elementFactory(post_it.id,post_it.type,post_it.text,post_it.axeX,post_it.axeY,post_it.couleur);
         
         for(let count=0;count<tabx.length;count++){
-            console.log("[",tabx[count],taby[count],"]");
+           // console.log("[",tabx[count],taby[count],"]");
         }
         id++;
+        console.log("Add to data base");
+        /*
+        function sendJSON(dataToStringify) {
+            var stringifiedData = JSON.stringify(dataToStringify);
+         
+            jQuery.ajax({
+               url: 'http://localhost:8080,
+               data: {stringified: stringifiedData},
+               success: function(data) {
+                  //code to handle successful AJAX post
+               },
+               error(XMLHttpRequest, textStatus, errorThrown) {
+                  //code to handle errors
+               }
+            });
+         }*/
+
     }
 
 }
@@ -150,6 +169,74 @@ function boucleDeselectionDePostit(){
     }
     requestAnimationFrame(boucleDeselectionDePostit);
 }
+/** Fonction pythagore
+ * @debrif cette fonction permet de calculer le côte d'un triangle rectangle qui applique le théorme de pythagore
+ * @param x le côte x du triangle
+ * @param y le côte y du triangle
+ */
+function pythagore(x,y){
+    return Math.round(Math.sqrt(Math.pow(x,2)+Math.pow(y,2)));
+}
+/** Fonction direction
+ * @debrif cette fonction permet de renvoyer la position sur un repère  rr signifie right right ll left left rb right bottom lb left bottom
+ * @param x la valeur du point x 
+ * @param y la valeur du point y
+ */ 
+function direction(x,y){
+    if(x>0 && y>0)
+       return "rr";
+    else if(x<0 && y>0)
+       return "ll"
+    else if(x>0 && y<0)
+       return "rb";
+    else
+       return "lb";
+ }
+ /** Fonction angleDegree
+  * @debrif cette fonction nous renvoie l'angle (dans un triangle rectangle )
+  * @param x valeur de cote x 
+  * @param y valeur de cote y
+  */
+function angleDegree(x,y) {
+   var angleRad = Math.atan(y/x);
+   var angleDeg = angleRad * 180 / Math.PI;
+   
+   return angleDeg;
+}
+/** Fonction angleSouris
+ * @debrif cette fonction renvoie l'angle de la souris par rapport à l'origine
+ * @param x position x 
+ * @param y position y
+ */
+function angleSouris(x,y){
+    return angleDegree(Math.abs(x),pythagore(x,y)); 
+}
+function target(e){
+    var axeX=Math.abs((window.innerWidth/2-e.clientX));
+    var axeY=Math.abs((window.innerHeight/2-e.clientY));
+    var naxeX=-Math.abs((window.innerWidth/2-e.clientX));
+    var naxeY=-Math.abs((window.innerHeight/2-e.clientY));
+ 
+    if(e.clientX<window.innerWidth/2 && e.clientY < window.innerHeight/2){
+       console.log(direction(naxeX,axeY));
+       console.log("degree ",angleSouris(naxeX,axeY));
+ 
+    }
+    else if(e.clientX<window.innerWidth/2 && e.clientY> window.innerHeight/2){
+       console.log(direction(naxeX,naxeY));
+       console.log("degree ",angleSouris(naxeX,naxeY));
+    }
+    else if(e.clientX>window.innerWidth/2 && e.clientY > window.innerHeight/2){
+       console.log(direction(axeX,naxeY));
+       console.log("degree ",angleSouris(axeX,naxeY));
+     
+    }
+    else{
+       console.log(direction(axeX,axeY));
+       console.log("degree ",angleSouris(axeX,axeY));
+ 
+    }
+}
 function main (){
 
     var newPost_it=new Postit();
@@ -157,15 +244,17 @@ function main (){
 
     var buttonAjouter=document.getElementById('Ajouter');
     buttonAjouter.addEventListener("click",newPost_it.ajoutPostit);
+
     var buttonDeconnexion=document.getElementById('Deconnexion');
     buttonDeconnexion.addEventListener("click",deconnexion);
     boucleDeselectionDePostit();
+
     var buttonSupprimer=document.querySelector("#Supprimer");
     buttonSupprimer.addEventListener('dragover',dragOver);
     buttonSupprimer.addEventListener('dragenter',dragEnter);
     buttonSupprimer.addEventListener('dragleave',dragLeave);
     buttonSupprimer.addEventListener('drop',dragDrop);
-    
+    window.addEventListener("mousemove", target);
 
 }
 
